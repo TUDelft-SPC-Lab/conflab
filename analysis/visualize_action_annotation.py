f89d6ccf731f5cadcc52409bff72845dcd2df3ac
Era Dorta <e.dorta@tudelft.nl>
2024-06-20 15:49:34 +0200

    Add support for showing multiple annotations

--- analysis/visualize_action_annotation.py
+++ analysis/visualize_action_annotation.py
@@ -37,22 +37,23 @@ def add_annotation_square_and_progress(
     frame_index: int,
     total_frames: int,
     annotations_img_height: int,
+    progress_img_height: int,
 ) -> np.ndarray:
     """
     Add two square at the bottom of the video showing the annotation values and the current index.
     Green shows an annotation with a value of 1, black with a value of 0 and red a missing annotation (nan value)
     """
     size_with_annotation = list(frame.shape)
-    size_with_annotation[0] += annotations_img_height * 2
+    size_with_annotation[0] += annotations_img_height + progress_img_height
     frame_with_annotations = np.zeros(size_with_annotation, dtype=frame.dtype)
     frame_height = frame.shape[0]
     frame_width = frame.shape[1]
 
     frame_with_annotations[:frame_height] = frame
 
-    frame_with_annotations[-annotations_img_height * 2 : -annotations_img_height] = (
-        annotation_square
-    )
+    frame_with_annotations[
+        -annotations_img_height - progress_img_height : -progress_img_height
+    ] = annotation_square
 
     # Transform with frame_index to the width of the image
     image_width_frame_index = (frame_index / total_frames) * (frame_width - 1)
@@ -63,7 +64,10 @@ def add_annotation_square_and_progress(
     cv2.rectangle(
         frame_with_annotations,
         (image_width_frame_index, frame_height + annotations_img_height),
-        (image_width_frame_index, frame_height + annotations_img_height * 2),
+        (
+            image_width_frame_index,
+            frame_height + annotations_img_height + progress_img_height,
+        ),
         color=(255, 0, 0),
     )
 
@@ -95,42 +99,58 @@ def create_annotation_square(
 
 def main():
     segment_name = "vid2-seg8"
+    annotators = [1, 2, 3]
+    participant = 22
+
+    # Size in pixels of the annotation square below the frames
+    annotations_img_height = 20
 
     video_path = Path(
         f"/home/era/code/NEON/data/conflab/video_segments/cam2/{segment_name}-scaled-denoised.mp4"
     )
-    annotations = Path(
-        f"/home/era/Downloads/exported_csv_files/Speaking/With_Audio/{segment_name.replace('-','_')}_ann1.csv"
-    )
-    participant = "2"
+
+    annotation_paths: list[Path] = []
+    for i in annotators:
+        annotation_paths.append(
+            Path(
+                f"/home/era/Downloads/exported_csv_files/Speaking/With_Audio/{segment_name.replace('-','_')}_ann{i}.csv"
+            )
+        )
 
     # Read the annotations
-    annotations_df = pd.read_csv(annotations)
-    data_for_participant = annotations_df[participant].array
+    annotations_dfs = [
+        pd.read_csv(annotation_path) for annotation_path in annotation_paths
+    ]
+    data_for_participant = [
+        annotations_df[str(participant)].array for annotations_df in annotations_dfs
+    ]
 
     cap = cv2.VideoCapture(video_path)
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     # Nearest neighbour interpolate data_for_participant to the frame width
     # Create a function defined as y = f(x)
-    interpolator_participant = scipy.interpolate.interp1d(
-        x=np.arange(0, len(data_for_participant)),
-        y=data_for_participant,
-        kind="nearest",  # Nearest neighbour interpolation
-        fill_value=np.nan,  # Extrapolate with nans
-        bounds_error=False,  # Allow extrapolation
-        assume_sorted=True,  # Data is sorted on x
-    )
+    interpolators_participant: list[scipy.interpolate.interp1d] = []
+    for data_for_participant_i in data_for_participant:
+        interpolators_participant.append(
+            scipy.interpolate.interp1d(
+                x=np.arange(0, len(data_for_participant_i)),
+                y=data_for_participant_i,
+                kind="nearest",  # Nearest neighbour interpolation
+                fill_value=np.nan,  # Extrapolate with nans
+                bounds_error=False,  # Allow extrapolation
+                assume_sorted=True,  # Data is sorted on x
+            )
+        )
     data_for_participant_in_frame_width = None
 
-    window_name = f"Participant {participant} {annotations.parent.parent.name} {annotations.parent.name}"
+    window_name = f"Participant {participant} {annotation_paths[0].parent.parent.name} {annotation_paths[0].parent.name}"
     cv2.namedWindow(window_name)
 
     def on_trackback_change(frame_index: int):
         _on_trackback_change(cap, frame_index)
 
     cv2.createTrackbar("Frame", window_name, 0, total_frames - 1, on_trackback_change)
-    annotations_img_height = 20 # Size in pixels of the annotation square below the frames
 
     last_frame, annotation_square = None, None
     while True:
@@ -141,17 +161,24 @@ def main():
 
         frame_index = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
 
-        if data_for_participant_in_frame_width is None:
+        if annotation_square is None:
             frame_width = frame.shape[1]
-            data_for_participant_in_frame_width = interpolator_participant(
-                np.linspace(0, len(data_for_participant), frame_width)
-            )
-            annotation_square = create_annotation_square(
-                annotations=data_for_participant_in_frame_width,
-                annotations_img_height=annotations_img_height,
-                frame_width=frame_width,
-                dtype=frame.dtype,
-            )
+            annotation_squares = []
+            for interpolators_participant, data_for_participant_i in zip(
+                interpolators_participant, data_for_participant
+            ):
+                data_for_participant_in_frame_width = interpolators_participant(
+                    np.linspace(0, len(data_for_participant_i), frame_width)
+                )
+                annotation_squares.append(
+                    create_annotation_square(
+                        annotations=data_for_participant_in_frame_width,
+                        annotations_img_height=annotations_img_height,
+                        frame_width=frame_width,
+                        dtype=frame.dtype,
+                    )
+                )
+            annotation_square = np.concatenate(annotation_squares, axis=0)
 
         last_frame = frame
         frame_with_annotations = add_annotation_square_and_progress(
@@ -159,13 +186,14 @@ def main():
             annotation_square,
             frame_index=frame_index,
             total_frames=total_frames,
-            annotations_img_height=annotations_img_height,
+            annotations_img_height=annotations_img_height * len(annotators),
+            progress_img_height=annotations_img_height,
         )
 
         cv2.imshow(window_name, frame_with_annotations)
         cv2.setTrackbarPos("Frame", window_name, frame_index)
 
-        # Wait the 1 millisecond for a keypress before moving to the next frame
+        # Wait 1 millisecond for a keypress before moving to the next frame
         key = cv2.waitKey(1)
         if key == ord("q"):
             break
